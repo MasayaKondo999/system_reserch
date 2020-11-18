795b8871ab64e33aad4d5d01850d0d1870251fcf
MasayaKondo <52480526+MasayaKondo999@users.noreply.github.com>
2020-11-18 21:16:48 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,9 +1,9 @@
 import tweepy
 # 取得したAPIキー
-consumer_key = '2NU4F8yPIfTVAXpSSaRZXmWcE'
-consumer_secret = 'BMild4HNIbQJghLiK1Towz7TJ6gZLiZTlVY0BpAOOii3fciWsQ'
-access_token = '1323787767470456832-5TSsMVDAGfq0i33Vxpyn5RI4S6CdV1'
-access_token_secret = 'AOniW4WEbERrt97RvQMBpQImde5ofdIwcL3rqt82Cu3RV'
+consumer_key = ''
+consumer_secret = ''
+access_token = ''
+access_token_secret = 'A'
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
